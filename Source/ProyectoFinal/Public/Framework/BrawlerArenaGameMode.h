5839a0a9f854a13ce4e6dd98ed5b823660cc3b1c
Luciano Benjamin Arguello <lucianobenjaminarguello@upc.edu.ar>
2026-05-18 19:57:25 -0300

    Creación de GameMode

---
+++ Source/ProyectoFinal/Public/Framework/BrawlerArenaGameMode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BrawlerArenaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOFINAL_API ABrawlerArenaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
