a00539d91c734e9985fa866786034ac739655217
Luciano Benjamin Arguello <lucianobenjaminarguello@upc.edu.ar>
2026-05-18 20:01:35 -0300

    Add PlayerState

---
+++ Source/ProyectoFinal/Public/Framework/BrawlerArenaGameState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BrawlerArenaGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOFINAL_API ABrawlerArenaGameState : public AGameState
+{
+	GENERATED_BODY()
+};
