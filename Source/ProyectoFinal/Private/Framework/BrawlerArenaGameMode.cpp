4b753be4e81d458e1c97244014dfad1e4e226b88
Luciano Benjamin Arguello <lucianobenjaminarguello@upc.edu.ar>
2026-05-20 00:20:35 -0300

    Comprueba la cantidad maxima de jugadores sino no deja entrar a mas, determina el jugador ganador contando los puntos del gamestate

--- Source/ProyectoFinal/Private/Framework/BrawlerArenaGameMode.cpp
+++ Source/ProyectoFinal/Private/Framework/BrawlerArenaGameMode.cpp
@@ -23,6 +23,22 @@ void ABrawlerArenaGameMode::BeginPlay()
 	GetWorldTimerManager().SetTimer(MatchTimer, this, &ABrawlerArenaGameMode::StartMatch, MatchStartTimerDuration, false);
 }
 
+void ABrawlerArenaGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
+	FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+	
+	// Tomado de la IA de unreal, deberia dispararse cuando hay un error de conexion por buenas practicas
+	if (!ErrorMessage.IsEmpty()) return;
+	
+	// Verificamos que la sala no este llena
+	if (GetNumPlayers() > MaxPlayers)
+	{
+		ErrorMessage = TEXT("Server is full. Elimination of connection attempt.");
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::Printf(TEXT("Se alcanzo el numero maximo de jugadores")));
+	}
+}
+
 void ABrawlerArenaGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
@@ -59,6 +75,7 @@ void ABrawlerArenaGameMode::AssignWarriorType(APlayerController* NewPlayer)
 	}
 }
 
+// Se dispara cuando el array que define a los guerreros se vació
 void ABrawlerArenaGameMode::RefillWarriorClassesPool()
 {
 	AvailableClassesPool.Empty();
@@ -67,4 +84,34 @@ void ABrawlerArenaGameMode::RefillWarriorClassesPool()
 	AvailableClassesPool.Add(EWarriorType::Archer);
 	
 }
+
+// Determina el ganador
+void ABrawlerArenaGameMode::DetermineWinner()
+{
+	AGameState* GS = GetGameState<ABrawlerArenaGameState>();
+	if (!GS) return;
+	
+	APlayerState* BestPlayer = nullptr;
+	float HighestScore = -1.f;
+	
+	// Iteramos sobre todos los PlayerStateConectados
+	for (APlayerState* PS : GS->PlayerArray)
+	{
+		// Unreal trae un sistema de Score integrado para poder usar rapidamente, lo podemos overraidear
+		if (PS && PS-> GetScore() > HighestScore)
+		{
+			HighestScore = PS->GetScore();
+			BestPlayer = PS;
+		}
+	}
+	
+	if (BestPlayer)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green, FString::Printf(TEXT("El ganador es %s"), *BestPlayer->GetName()));
+		// Crear un widget que muestre la condicion de victoria?
+		// GS->Multicast_OnGameEnded(BestPlayer); para que muestre el widget 
+		EndMatch();
+	}
+}
+
 
--- Source/ProyectoFinal/Public/Framework/BrawlerArenaGameMode.h
+++ Source/ProyectoFinal/Public/Framework/BrawlerArenaGameMode.h
@@ -21,6 +21,8 @@ public:
 	
 	virtual void BeginPlay() override;
 	
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+	
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 	
 	// Aca deberia definir como es que se gana y que hacer al respecto
@@ -30,19 +32,36 @@ public:
 	
 	FTimerHandle MatchTimer;
 	
-	UPROPERTY(EditDefaultsOnly, Category="GameMode")
+	UPROPERTY(EditDefaultsOnly, Category="Match Config")
 	float MatchStartTimerDuration = 5;
 	
-	// Cuanto dura la partida?
-	UPROPERTY(EditDefaultsOnly, Category="GameMode")
-	float MatchTotalTimerDuration;
+	// Cuantas olas son las que se van a desarrollar en la partida
+	UPROPERTY(EditDefaultsOnly, Category="Match Config")
+	int32 EnemyWaves = 3; 	
+	
+	// Numero de olas restantes
+	UPROPERTY()
+	int32 RemainingWaves;
+	
+	// Número maximo de jugadores en el match
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Match Config")
+	int32 MaxPlayers;
+	
 private:
 	
 	UFUNCTION(BlueprintCallable, Category="Warrior Class")
 	void AssignWarriorType(APlayerController* NewPlayer);
 	
+	UFUNCTION()
 	void RefillWarriorClassesPool();
+	
+	// Esta funcion se trigueara cuando RemainingWaves llegue a cero, es decir cuando no queden mas waves
+	UFUNCTION()
+	void DetermineWinner();
+
 	
 	UPROPERTY()
 	TArray<EWarriorType> AvailableClassesPool;
+	
+
 };
